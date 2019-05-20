7582b4b646a6dbe222c5dbbaae8fde91abe102f8
Shreyas Tallamraju <shreyastallamraju2020@u.northwestern.edu>
2019-05-20 15:49:30 -0500

    added a mentor notes list

--- client/src/SingleTicketComponents/Suggestions.js
+++ client/src/SingleTicketComponents/Suggestions.js
@@ -7,6 +7,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import ReactHtmlParser from 'react-html-parser';
 
 const classes = {
@@ -41,6 +42,12 @@ const classes = {
     height: '200px',
 
   },
+  mentorNotesField: {
+    position: 'relative',
+    marginTop: '10px',
+    width: '100%',
+
+  },
 };
 
 const Suggestions = ({classes, ticket}) => {
@@ -48,7 +55,7 @@ const Suggestions = ({classes, ticket}) => {
 
   const [sug0, setSug0] = useState("...")
   const [sug1, setSug1] = useState("...")
-
+  const [mentorNotes, setMentorNotes] = useState([ReactHtmlParser("This problem usually happens when student misspell fcn names<br />-M"), ReactHtmlParser("ask students to go through the lecture 2 slides if they ask you about variables<br />-J")]);
   // TODO: Map the suggestions properly instead of naïvely assuming that there will be only two
 
   const getSuggestions = () =>
@@ -56,9 +63,16 @@ const Suggestions = ({classes, ticket}) => {
     setSug0(ReactHtmlParser(ticket['response']['0']));
     setSug1(ReactHtmlParser(ticket['response']['1']));
   }
+
+  const getMentorNotes = () => 
+  {
+    //setMentorNotes([ReactHtmlParser(ticket['notes']['0']), ReactHtmlParser(ticket['notes']['1'])]);
+  }
+
   useEffect(() =>
   {
     getSuggestions();
+    getMentorNotes();
   },[ticket]);
 
   return (
@@ -88,8 +102,30 @@ const Suggestions = ({classes, ticket}) => {
                 </Paper>
         </List>
       </div>
-
-      
+      <br /> <br />
+      <div className={classes.typographyDiv}>
+        <Typography variant="h5" className={classes.typography}>
+          Mentor Notes
+        </Typography>
+      </div>
+      <div className={classes.allSuggestions}>
+        <List>
+                <Paper className={classes.suggestionPaper} elevation = {2}>
+                  <ListItem>
+                    <ListItemText
+                      primary={mentorNotes[0]}
+                      />
+                    </ListItem>
+                </Paper>
+                <Paper className={classes.suggestionPaper} elevation = {2}>
+                    <ListItem>
+                      <ListItemText
+                        primary={mentorNotes[1]}
+                      />
+                      </ListItem>
+                </Paper>
+        </List>
+      </div>
 
 
     </div>
